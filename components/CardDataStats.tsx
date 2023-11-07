1db53eb58cd9cb352f9c63f60db7485cd69a63c5
Shahriar Konok <srkonok20@gmail.com>
2023-11-07 15:58:22 +0600

    fetch dat successfull

--- components/CardDataStats.tsx
+++ components/CardDataStats.tsx
@@ -1,8 +1,8 @@
 import React, { ReactNode } from 'react';
 
 interface CardDataStatsProps {
+  post: string;
   title: string;
-  total: string;
   rate: string;
   levelUp?: boolean;
   levelDown?: boolean;
@@ -11,24 +11,24 @@ interface CardDataStatsProps {
 
 const CardDataStats: React.FC<CardDataStatsProps> = ({
   title,
-  total,
+  post,
   rate,
   levelUp,
   levelDown,
   children,
 }) => {
   return (
-    <div className="rounded-sm border border-stroke bg-white py-6 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
-      <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
+    <div className="rounded-sm border border-stroke bg-white py-8 px-7.5 shadow-default dark:border-strokedark dark:bg-boxdark">
+      {/* <div className="flex h-11.5 w-11.5 items-center justify-center rounded-full bg-meta-2 dark:bg-meta-4">
         {children}
-      </div>
+      </div> */}
 
       <div className="mt-4 flex items-end justify-between">
         <div>
-          <h4 className="text-title-md font-bold text-black dark:text-white">
-            {total}
+          <h4 className="text-post-md font-bold text-black dark:text-white">
+            {title}
           </h4>
-          <span className="text-sm font-medium">{title}</span>
+          <span className="text-sm font-medium">{post.slice(0,120)}</span>
         </div>
 
         <span
@@ -40,18 +40,19 @@ const CardDataStats: React.FC<CardDataStatsProps> = ({
 
           {levelUp && (
             <svg
-              className="fill-meta-3"
-              width="10"
-              height="11"
-              viewBox="0 0 10 11"
-              fill="none"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M4.35716 2.47737L0.908974 5.82987L5.0443e-07 4.94612L5 0.0848689L10 4.94612L9.09103 5.82987L5.64284 2.47737L5.64284 10.0849L4.35716 10.0849L4.35716 2.47737Z"
-                fill=""
-              />
-            </svg>
+            className="fill-meta-3"
+            width="10"
+            height="11"
+            viewBox="0 0 10 11"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M9.09103 5.82987L5 10.6911L4.35716 10.0849L4.35716 2.47737L9.09103 5.82987Z"
+              fill=""
+            />
+          </svg>
+        
           )}
           {levelDown && (
             <svg
