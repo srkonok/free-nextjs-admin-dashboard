541e170ce887ae60ac9dbbda9379418221f5de4c
Shahriar Konok <srkonok20@gmail.com>
2023-11-08 09:38:30 +0600

    pagionation done successfull

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -1,37 +1,52 @@
-"use client"
+"use client";
 import React, { useEffect, useState } from "react";
 import CardDataStats from "../CardDataStats";
 
 const ECommerce: React.FC = () => {
   const [data, setData] = useState([]); // Initialize data as an empty array
-  const currentPage=1;
-  useEffect(() => {
-    async function fetchData() {
-      try {
-        const response = await fetch("https://dummyjson.com/posts");
-        if (response.ok) {
-          const data = await response.json();
-          console.log(data);
-          setData(data.posts.slice(0,9));
-        } else {
-          console.error("Failed to fetch data");
-        }
-      } catch (error) {
-        console.error("An error occurred while fetching data", error);
-      }
-    }
+  const [allPosts, setAllPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [itemsPerPage] = useState(9); // Number of items to display per page
+  const [isLoading, setIsLoading] = useState(true);
 
-    fetchData();
-  }, []);
+  async function fetchData(newPage) {
+    const startIndex = (newPage - 1) * itemsPerPage;
+    const endIndex = startIndex + itemsPerPage;
+    try {
+      const response = await fetch("https://dummyjson.com/posts");
+      if (response.ok) {
+        const data = await response.json();
+        const paginatedData = data.posts.slice(startIndex, endIndex);
+        setData(data.posts);
+        setAllPosts(paginatedData);
+        setIsLoading(false);
+      } else {
+        console.error("Failed to fetch data");
+      }
+    } catch (error) {
+      console.error("An error occurred while fetching data", error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData(currentPage);
+  }, [currentPage]);
+
+  const handlePageChange = (newPage) => {
+    if (newPage >= 1 && newPage <= Math.ceil(data.length / itemsPerPage)) {
+      setCurrentPage(newPage);
+      fetchData(newPage); // Fetch data for the new page
+    }
+  };
 
   return (
     <>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 xl:grid-cols-3 2xl:gap-7.5">
-        {data.map((posts) => (
+        {allPosts.map((post) => (
           <CardDataStats
-            key={posts.id} // Assuming the 'id' property exists in your data
-            title={posts.title}
-            post={posts.body}
+            key={post.id} // Assuming the 'id' property exists in your data
+            title={post.title}
+            post={post.body}
             rate="read"
             levelUp
           />
@@ -39,9 +54,9 @@ const ECommerce: React.FC = () => {
       </div>
       <div className="flex justify-between items-center mt-4">
         <button
-          // onClick={() => handlePageChange(currentPage - 1)}
-          // disabled={currentPage === 1}
-          className={`bg-blue-500 text-white font-semibold py-2 px-4 rounded ${
+          onClick={() => handlePageChange(currentPage - 1)}
+          disabled={currentPage === 1}
+          className={`bg-blue-500 font-semibold py-2 px-4 rounded ${
             currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-700"
           }`}
         >
@@ -49,14 +64,13 @@ const ECommerce: React.FC = () => {
         </button>
         <span>{currentPage}</span>
         <button
-        // className={`bg-blue-500 text-white font-semibold py-2 px-4 rounded `}
-          // onClick={() => handlePageChange(currentPage + 1)}
-          // disabled={currentPage >= Math.ceil(allPosts.length / itemsPerPage)}
-          //   currentPage >= Math.ceil(allPosts.length / itemsPerPage)
-          //     ? "opacity-50 cursor-not-allowed"
-          //     : "hover:bg-blue-700"
-          // }`
-        
+          onClick={() => handlePageChange(currentPage + 1)}
+          disabled={currentPage >= Math.ceil(data.length / itemsPerPage)}
+          className={`bg-blue-500 font-semibold py-2 px-4 rounded ${
+            currentPage >= Math.ceil(data.length / itemsPerPage)
+              ? "opacity-50 cursor-not-allowed"
+              : "hover:bg-blue-700"
+          }`}
         >
           Next
         </button>
