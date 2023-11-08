1eee2e9cbba352baceb760bbabc883d9237eff2e
Shahriar Konok <srkonok20@gmail.com>
2023-11-08 08:42:13 +0600

    static pagination

--- components/Dashboard/E-commerce.tsx
+++ components/Dashboard/E-commerce.tsx
@@ -4,7 +4,7 @@ import CardDataStats from "../CardDataStats";
 
 const ECommerce: React.FC = () => {
   const [data, setData] = useState([]); // Initialize data as an empty array
-
+  const currentPage=1;
   useEffect(() => {
     async function fetchData() {
       try {
@@ -12,7 +12,7 @@ const ECommerce: React.FC = () => {
         if (response.ok) {
           const data = await response.json();
           console.log(data);
-          setData(data.posts);
+          setData(data.posts.slice(0,9));
         } else {
           console.error("Failed to fetch data");
         }
@@ -37,7 +37,30 @@ const ECommerce: React.FC = () => {
           />
         ))}
       </div>
-
+      <div className="flex justify-between items-center mt-4">
+        <button
+          // onClick={() => handlePageChange(currentPage - 1)}
+          // disabled={currentPage === 1}
+          className={`bg-blue-500 text-white font-semibold py-2 px-4 rounded ${
+            currentPage === 1 ? "opacity-50 cursor-not-allowed" : "hover:bg-blue-700"
+          }`}
+        >
+          Previous
+        </button>
+        <span>{currentPage}</span>
+        <button
+        // className={`bg-blue-500 text-white font-semibold py-2 px-4 rounded `}
+          // onClick={() => handlePageChange(currentPage + 1)}
+          // disabled={currentPage >= Math.ceil(allPosts.length / itemsPerPage)}
+          //   currentPage >= Math.ceil(allPosts.length / itemsPerPage)
+          //     ? "opacity-50 cursor-not-allowed"
+          //     : "hover:bg-blue-700"
+          // }`
+        
+        >
+          Next
+        </button>
+      </div>
       <div className="mt-4 grid grid-cols-12 gap-4 md:mt-6 md:gap-6 2xl:mt-7.5 2xl:gap-7.5">
         {/* Add other components or elements here */}
       </div>
